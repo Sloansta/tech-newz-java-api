313896eedda7b99bac72322f6b80fd4599b42579
Sloan <Sloan.boyce@gmail.com>
2022-03-26 12:07:53 -0400

    added basic interfaces for the repository

---
+++ src/main/java/com/technewz/repository/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.technewz.repository;
+
+import com.technewz.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface VoteRepository extends JpaRepository<Vote, Integer> {
+
+    @Query("SELECT count(*) FROM Vote v where v.postId = :id")
+    int countVotesByPostId(@Param("id") Integer id);
+}
